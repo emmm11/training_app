38383e54789dc1405f454725be23858f3b985d39
Emma Giles <emmagiles@Emmas-MacBook-Pro.local>
2019-07-20 15:55:30 -0400

    adding local storage of checked days

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 
 // working on saving the colours in local storage 
 // and retreiving so that all data is kept
-background = localStorage.backgroundColor;
-document.style
-function complete(btn, event, color) {
-    if (btn.style.backgroundColor == "pink") {
-        btn.style.backgroundColor = "white"
-    }
-    else {
-        btn.style.backgroundColor = "pink"
-    }
-    localStorage.setItem('backgroundColor', color);
 
+button = document.getElementById("dayOne");
+button.style.backgroundColor = localStorage.getItem('buttonColor');
+
+function complete () {
+
+    if(button.style.backgroundColor == "pink") {
+        button.style.backgroundColor = "white"
+        localStorage.setItem('buttonColor', 'white')
+    }
+
+    else {
+        button.style.backgroundColor = "pink"
+        localStorage.setItem('buttonColor', 'pink')
+    }
 }
+
+
+
+
+//function complete(btn, event, color) {
+  //  if (btn.style.backgroundColor == "pink") {
+    //    btn.style.backgroundColor = "white"
+      //  localStorage.setItem(btn.style.backgroundColor, 'white')
+    //}
+    //else {
+      //  btn.style.backgroundColor = "pink"
+        //localStorage.setItem(btn.style.backgroundColor, 'pink')
+    //}
+
+//}
 
