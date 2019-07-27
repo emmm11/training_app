76b12da8b002ac45a7cce17eefd322d5782083f5
Emma Giles <emmagiles@Emmas-MacBook-Pro.local>
2019-07-26 20:43:05 -0700

    finishing up mobile optimization

--- script.js
+++ script.js
@@ -1,37 +1,42 @@
-// *to-do* create all the day tiles (but what about filling them in with data? Can I match them up looping through and concatenating?)
 
 // Progress variables
 var doneCount = 0;
-var totalCount = 7; // update to total number of days
+var totalCount = 14; // update to total number of days
+var progressBar = document.getElementById("progressBar");
 
 // pulling 'done' days out of local storage from last use
-for(var j = 1; j < 8; j++) {
+for(var j = 1; j < 15; j++) {
     button = document.getElementById('day-' + j);
     button.style.backgroundColor = localStorage.getItem('colorButton-' + j);
     if(button.style.backgroundColor == 'pink') {
         doneCount += 1;
     }
+    progress();
 }
 // Progress stuff *to-do* update on click!
-var progressBar = document.getElementById("progressBar");
-progressBar.innerHTML = Math.round((doneCount/totalCount) * 100) + "% complete!";
+function progress() {
+    progressBar.innerHTML = Math.round((doneCount/totalCount) * 100) + "% complete!";
+}
 
 // changing the day colour when clicked for 'done'
 function complete(btn, event, color) {
     if (btn.style.backgroundColor == "pink") {
         btn.style.backgroundColor = "white";
+        doneCount -= 1;
 
     }
     else {
         btn.style.backgroundColor = "pink"
+        doneCount += 1;
 
     }
     // Updating local storage after colours are changed
     // this could be more efficient, shouldn't loop through them all for just one click
-    for(var k = 1; k < 8; k++) {
+    for(var k = 1; k < 15; k++) {
         button = document.getElementById('day-' + k);
         localStorage.setItem('colorButton-' + k, button.style.backgroundColor);
     }
+    progress();
 }
 
 
