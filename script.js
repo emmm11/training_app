eeaf73b077ec627fe446f609ad9e3a29c7a47686
Emma Giles <emmagiles@Emmas-MacBook-Pro.local>
2019-07-26 19:49:19 -0700

    saving to local storage

--- script.js
+++ script.js
@@ -1,35 +1,38 @@
+// *to-do* create all the day tiles (but what about filling them in with data? Can I match them up looping through and concatenating?)
 
-// working on saving the colours in local storage 
-// and retreiving so that all data is kept
+// Progress variables
+var doneCount = 0;
+var totalCount = 7; // update to total number of days
 
-button = document.getElementById("dayOne");
-button.style.backgroundColor = localStorage.getItem('buttonColor');
-
-function complete () {
-
-    if(button.style.backgroundColor == "pink") {
-        button.style.backgroundColor = "white"
-        localStorage.setItem('buttonColor', 'white')
+// pulling 'done' days out of local storage from last use
+for(var j = 1; j < 8; j++) {
+    button = document.getElementById('day-' + j);
+    button.style.backgroundColor = localStorage.getItem('colorButton-' + j);
+    if(button.style.backgroundColor == 'pink') {
+        doneCount += 1;
     }
+}
+// Progress stuff *to-do* update on click!
+var progressBar = document.getElementById("progressBar");
+progressBar.innerHTML = Math.round((doneCount/totalCount) * 100) + "% complete!";
 
+// changing the day colour when clicked for 'done'
+function complete(btn, event, color) {
+    if (btn.style.backgroundColor == "pink") {
+        btn.style.backgroundColor = "white";
+
+    }
     else {
-        button.style.backgroundColor = "pink"
-        localStorage.setItem('buttonColor', 'pink')
+        btn.style.backgroundColor = "pink"
+
+    }
+    // Updating local storage after colours are changed
+    // this could be more efficient, shouldn't loop through them all for just one click
+    for(var k = 1; k < 8; k++) {
+        button = document.getElementById('day-' + k);
+        localStorage.setItem('colorButton-' + k, button.style.backgroundColor);
     }
 }
 
 
-
-
-//function complete(btn, event, color) {
-  //  if (btn.style.backgroundColor == "pink") {
-    //    btn.style.backgroundColor = "white"
-      //  localStorage.setItem(btn.style.backgroundColor, 'white')
-    //}
-    //else {
-      //  btn.style.backgroundColor = "pink"
-        //localStorage.setItem(btn.style.backgroundColor, 'pink')
-    //}
-
-//}
 
